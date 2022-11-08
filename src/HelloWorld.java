4874ed1b005491c4dc4229af7a2fe5c0f28ee59c
Brittney Brinson <brittneyc95@outlook.com>
2022-11-08 12:48:00 -0500

    Java exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,28 @@
 public class HelloWorld {
     public static void main (String[] args){
-        System.out.print("Hello, World!");
+//        System.out.print("Hello, World!");
+//        int myFavoriteNumber = 3;
+//        System.out.print(myFavoriteNumber);
+
+//        String myString = "Chainsaw man is a great anime";
+//        System.out.println(myString);
+//
+//        double myNumber = 3.14;        //floats need an f character with the value
+////        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+////        System.out.println(x);  // the first code block with the post increment, adds one to the variable after it's called. the second block adds one as it's called.
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+        int x = 4 + 5;
+        int y = 3 * 4;
+
+
     }
 }
+
