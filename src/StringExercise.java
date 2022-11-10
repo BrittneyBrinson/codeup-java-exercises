919d8b359877b58cdef67d6103cf08fe92934433
Brittney Brinson <brittneyc95@outlook.com>
2022-11-10 18:29:55 -0500

    Made string things Java progress

---
+++ src/StringExercise.java
@@ -0,0 +1,8 @@
+public class StringExercise {
+    public static void main(String[] args){
+        String thing = "We don't need ";
+        String stuff = "no education";
+        String stuff2 = "no thought control";
+            System.out.println(thing + stuff);
+    }
+}
