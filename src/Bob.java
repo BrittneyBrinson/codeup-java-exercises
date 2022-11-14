0ac6342e631972da1ad0814d62dcd361f7cf6291
Brittney Brinson <brittneyc95@outlook.com>
2022-11-14 10:48:30 -0500

    Finished all of Java String exercises

---
+++ src/Bob.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Bob {
+    public static void main(String[] args){
+        System.out.println("Hey, I'm Bob ask me something or just talk to me");
+
+            Scanner scanner = new Scanner(System.in);
+
+        String userInput = scanner.nextLine();
+
+            if(userInput.endsWith("?")){
+                System.out.println("Bob: yeah sure mhm");
+            }
+            if(userInput.endsWith("!")){
+                System.out.println("Bob: HEY, YOU need to calm down! ");
+            }
+            if(userInput.length() == 1){
+                System.out.println("Wow, okay... nice talk..");
+            }
+            else {
+                System.out.println("mhm Whatever");
+            }
+
+
+
+
+    }
+}
