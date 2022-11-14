ddb8b7f2a9e6fa89ce32ed1b365d2cd827d8d436
Brittney Brinson <brittneyc95@outlook.com>
2022-11-14 10:24:53 -0500

    Finished exercise 1 of Java Strings

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class ControlFlowExercises {
     public static void main(String[] args) {
 //        int i = 5;
+        
 //        while (i <= 15){
 //            System.out.println( i++);
 //        }
@@ -19,13 +20,17 @@ public class ControlFlowExercises {
 //            }while( i <= 1000);
 
 //            for(int i = 0; i <= 100; i++){
+
 //                if(i % 3 == 0 & i % 5 == 0){
+
 //                    System.out.println("FizzBuzz " + i);
 //                }
 //                    else if(i % 5 == 0){
+
 //                        System.out.println("Buzz " + i);
 //                }
 //                else if(i % 3 == 0){
+
 //                    System.out.println("Fizz " + i);
 //                }
 //                    else System.out.println(i);
@@ -53,49 +58,53 @@ public class ControlFlowExercises {
 //
 //        String stopStart = scanner.nextLine();
 //                if(stopStart.equals( "yes")){
+
 //                    System.out.println("Enter another integer");
+
 //                    int userInput2 = Integer.parseInt(scanner.nextLine());
 //
 //                    for (int i = 0; i <= userInput2; i++) {
+
 //                        if (i == i) {
+
 //                            System.out.println(i + " | " + i * i + " | " + Math.pow(i, 3));
 //                        }
 //                    }
 //                }
-        System.out.println("Enter a NUMERICAL grade from 0 - 100, please");
-
-            int userNumber = Integer.parseInt(scanner.nextLine());
-
-        System.out.println("Do you want to continue? if so type yes, otherwise type no");
-
-            String goForward = scanner.nextLine();
-
-        if(goForward.equals("no")){
-            System.out.println("Thank you for participating, have a nice day! :) ");
-            }
-                if(goForward.equals("yes")){
-
-                   if (userNumber < 100 & userNumber > 88){
-                       System.out.println("Your number: " + userNumber + " is in the A grading range");
-                   }
-
-                   else if(userNumber < 87 & userNumber > 80){
-                       System.out.println("Your number: " + userNumber + " is in the B grading range");
-                   }
-
-                   else if(userNumber < 79 & userNumber > 67){
-                        System.out.println("Your number: " + userNumber + " is in the C grading range");
-                    }
-
-                   else if(userNumber < 66 & userNumber > 60){
-                        System.out.println("Your number: " + userNumber + " is in the D grading range");
-                    }
-
-                   else if(userNumber < 59 & userNumber > 0){
-                        System.out.println("Your number: " + userNumber + " is in the F grading range");
-                    }
-
-                  }
-
+//        System.out.println("Enter a NUMERICAL grade from 0 - 100, please");
+//
+//            int userNumber = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.println("Do you want to continue? if so type yes, otherwise type no");
+//
+//            String goForward = scanner.nextLine();
+//
+//        if(goForward.equalsIgnoreCase("no")){
+//            System.out.println("Thank you for participating, have a nice day! :) ");
+//            }
+//                if(goForward.equalsIgnoreCase("yes")){
+//
+//                   if (userNumber < 100 & userNumber > 88){
+//                       System.out.println("Your number: " + userNumber + " is in the A grading range");
+//                   }
+//
+//                   else if(userNumber < 87 & userNumber > 80){
+//                       System.out.println("Your number: " + userNumber + " is in the B grading range");
+//                   }
+//
+//                   else if(userNumber < 79 & userNumber > 67){
+//                        System.out.println("Your number: " + userNumber + " is in the C grading range");
+//                    }
+//
+//                   else if(userNumber < 66 & userNumber > 60){
+//                        System.out.println("Your number: " + userNumber + " is in the D grading range");
+//                    }
+//
+//                   else if(userNumber < 59 & userNumber > 0){
+//                        System.out.println("Your number: " + userNumber + " is in the F grading range");
+//                    }
+//
+//                  }
+//
                 }
         }
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,8 +1,23 @@
 public class StringExercise {
     public static void main(String[] args){
         String thing = "We don't need ";
-        String stuff = "no education";
-        String stuff2 = "no thought control";
-            System.out.println(thing + stuff);
+
+            System.out.println(thing + "no education");
+
+            System.out.println(thing + "no thought control");
+
+        String check = "Check \"this\"";
+
+            System.out.println(check +  " out!," + "\"s inside of \"s!");
+
+        String drive = "In windows, the main drive is ";
+
+            System.out.println(drive + "usually C:\\ ");
+
+        String bigBack = "I can do backslashes ";
+
+            System.out.println(bigBack + "\\, double backslashes \\\\, and the amazing triple backslash \\\\\\ !" );
+
+
     }
 }
