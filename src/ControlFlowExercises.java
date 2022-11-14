8c8faa8f787cd0d0c92ffdacc44d98d132c82428
Brittney Brinson <brittneyc95@outlook.com>
2022-11-13 21:37:42 -0500

    Finished control statements and loops

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -31,40 +31,71 @@ public class ControlFlowExercises {
 //                    else System.out.println(i);
 //            }
         Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println("Enter a integer");
+//
+//        int userInput = Integer.parseInt(scanner.nextLine());
+//
+//        System.out.println("Here is your table!");
+//
+//        System.out.println(" ");
+//
+//        System.out.println("number | squared | cubed");
+//
+//        System.out.println("------ | ------- | -----");
+//
+//        for (int i = 0; i <= userInput; i++) {
+//                if (i == i) {
+//                    System.out.println(i + " | " + i * i + " | " + Math.pow(i, 3));
+//                }
+//            }
+//        System.out.println("If you want to continue, type yes. Otherwise, type no.");
+//
+//        String stopStart = scanner.nextLine();
+//                if(stopStart.equals( "yes")){
+//                    System.out.println("Enter another integer");
+//                    int userInput2 = Integer.parseInt(scanner.nextLine());
+//
+//                    for (int i = 0; i <= userInput2; i++) {
+//                        if (i == i) {
+//                            System.out.println(i + " | " + i * i + " | " + Math.pow(i, 3));
+//                        }
+//                    }
+//                }
+        System.out.println("Enter a NUMERICAL grade from 0 - 100, please");
 
-        System.out.println("Enter a integer");
+            int userNumber = Integer.parseInt(scanner.nextLine());
 
-        int userInput = Integer.parseInt(scanner.nextLine());
+        System.out.println("Do you want to continue? if so type yes, otherwise type no");
 
-        System.out.println("Here is your table!");
+            String goForward = scanner.nextLine();
 
-        System.out.println(" ");
-
-        System.out.println("number | squared | cubed");
-
-        System.out.println("------ | ------- | -----");
-
-        for (int i = 0; i <= userInput; i++) {
-                if (i == i) {
-                    System.out.println(i + " | " + i * i + " | " + Math.pow(i, 3));
-                }
+        if(goForward.equals("no")){
+            System.out.println("Thank you for participating, have a nice day! :) ");
             }
-        System.out.println("If you want to continue, type yes. Otherwise, type no.");
+                if(goForward.equals("yes")){
 
-        String stopStart = scanner.nextLine();
-                if(stopStart.equals( "yes")){
-                    System.out.println("Enter another integer");
-                    int userInput2 = Integer.parseInt(scanner.nextLine());
+                   if (userNumber < 100 & userNumber > 88){
+                       System.out.println("Your number: " + userNumber + " is in the A grading range");
+                   }
 
-                    for (int i = 0; i <= userInput2; i++) {
-                        if (i == i) {
-                            System.out.println(i + " | " + i * i + " | " + Math.pow(i, 3));
-                        }
+                   else if(userNumber < 87 & userNumber > 80){
+                       System.out.println("Your number: " + userNumber + " is in the B grading range");
+                   }
+
+                   else if(userNumber < 79 & userNumber > 67){
+                        System.out.println("Your number: " + userNumber + " is in the C grading range");
                     }
+
+                   else if(userNumber < 66 & userNumber > 60){
+                        System.out.println("Your number: " + userNumber + " is in the D grading range");
+                    }
+
+                   else if(userNumber < 59 & userNumber > 0){
+                        System.out.println("Your number: " + userNumber + " is in the F grading range");
+                    }
+
+                  }
+
                 }
-
-
-
-
         }
-    }
