a7185baaedfbb6d1d73067316ccdf1578a82b7cb
Brittney Brinson <brittneyc95@outlook.com>
2022-11-20 22:51:25 -0500

    Progress

--- src/util/Input.java
+++ src/util/Input.java
@@ -2,18 +2,38 @@ package util;
 import java.util.Scanner;
 
 public class Input{
-    private Scanner scanner;
-    public Input(Scanner scanner){         //Input constructor
-        this.scanner = new Scanner(System.in);
+    private Scanner sc;
+
+
+    public Input(){         //Input constructor
+        this.sc = new Scanner(System.in);
     }
         String getString(Scanner Obj){
-        return this.scanner.toString();
+        return this.sc.toString();
     }
 
     boolean yesNo(String userInput) {
-        userInput.equals("yes");
-        return true;
+       boolean newOutput = userInput.equals("yes") || userInput.equals("y") ? true : false;
+       return newOutput;
     }
+
+    int getInt(int min, int max){
+        System.out.println("Enter a number between 50 - 100");
+        min = 50;
+        max = 100;
+        int grabInput = Integer.parseInt(sc.nextLine());
+
+        if(grabInput > 100 && grabInput < 50){
+            throw new IllegalArgumentException("Please enter a number that is BETWEEN 50-100");
+        }
+
+        return grabInput;
+
+        }
+
+
+
+
 
 
 
