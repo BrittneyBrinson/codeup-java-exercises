f76b44fc88901f97d16f58b95f1a9c3e2dc10438
Brittney Brinson <brittneyc95@outlook.com>
2022-12-05 17:22:08 -0500

    Finished Java Arrays exercises

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,10 +1,6 @@
 package movies;
+
 import util.Input;
-
-import java.util.Arrays;
-import java.util.Scanner;
-
-
 public class MoviesApplication {
 
     public static void main(String[] args) {
@@ -16,22 +12,47 @@ public class MoviesApplication {
                 "\n Enter option 4:view movies in the horror category!" +
                 "\n Enter option 5:view movies in the scifi category!");
         Input userInput = new Input();
-        var scanner = new Scanner(System.in);
-        int finalInput = scanner.nextInt();
+        int finalInput = userInput.getInt();
+        System.out.println(finalInput);
         while (true) {
             if (finalInput == 1) {
                 for (int i = 0; i < MoviesArray.findAll().length; i++) {
                     System.out.println(MoviesArray.findAll()[i].Movie() + "--" + MoviesArray.findAll()[i].getCategory());  //I called the MoviesArray class and with dot notation I accessed its STATIC method.
-                        }
-                break;
                 }
-//                else if(finalInput == 2) {
-//                   String animatedCategory = "animated";
-//                    break;
-          }
+                break;
+            } else if (finalInput == 2) {
+                for (Movie animatedOnly : MoviesArray.findAll()) {
+                    if (animatedOnly.getCategory().equals("animated")) {
+                        System.out.println(animatedOnly.Movie());
+                    }
+                }
+                break;
+            } else if (finalInput == 3) {
+                for (Movie dramaOnly : MoviesArray.findAll()) {
+                    if (dramaOnly.getCategory().equals("drama")) {
+                        System.out.println(dramaOnly.Movie());
+                    }
+                }
+                break;
+            } else if (finalInput == 4) {
+                for (Movie horrorOnly : MoviesArray.findAll()) {
+                    if (horrorOnly.getCategory().equals("horror")) {
+                        System.out.println(horrorOnly.Movie());
+                    }
+                }
+                break;
+            } else if (finalInput == 5) {
+                for (Movie scifiOnly : MoviesArray.findAll()) {
+                    if (scifiOnly.getCategory().equals("scifi")) {
+                        System.out.println(scifiOnly.Movie());
+                    }
+                }
+                break;
+            }
         }
 
 
     }
+}
 
 
