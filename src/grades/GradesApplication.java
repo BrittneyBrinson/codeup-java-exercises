dfd4b2996004e577580780905f211a795f4efc16
Brittney Brinson <brittneyc95@outlook.com>
2022-12-11 14:24:05 -0500

    Finished Grades Application for JAVA

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,4 +1,5 @@
 package grades;
+
 import java.util.Scanner;
 import java.util.HashMap;
 
@@ -31,37 +32,67 @@ public class GradesApplication {
         students.put("NeonStars1", studentD);
 
         System.out.println("Here are the Github usernames of the students");
-        System.out.print("BlueSky45  |");
-        System.out.print("GreenPlatypus8  |");
-        System.out.print("VeggieTales9  |");
-        System.out.println("NeonStars1");
-
-        System.out.println("Which student would you like to see more information about?");
-        Scanner scanner = new Scanner(System.in);
-        String teacherSelection = scanner.nextLine();
-
-        //LOOK INTO RECURSION!
-
-
-
-            if(teacherSelection.equals("BlueSky45"))
-            {
+        System.out.print(" BlueSky45  |");
+        System.out.print(" GreenPlatypus8  |");
+        System.out.print(" VeggieTales9  |");
+        System.out.println(" NeonStars1");
+        while (true) {
+            System.out.println("Which student would you like to see more information about?");
+            Scanner scanner = new Scanner(System.in);
+            String teacherSelection = scanner.nextLine();
+            if (teacherSelection.equalsIgnoreCase("BlueSky45")) {
                 System.out.println("Name: " + studentA.getName());
                 System.out.println("Grades: " + studentA.getGrades());
-                System.out.println("Grade-point Average: " + studentA.getGradeAverage());
+                System.out.println("Current Average: " + studentA.getGradeAverage());
+                System.out.println("Would you like to see another student?");
+                String keepGoing = scanner.nextLine();
+                if (keepGoing.equals("yes")) {
+                    continue;
+                } else {
+                    System.out.println("Goodbye! Have a nice day!");
+                    break;
+                }
+            } else if (teacherSelection.equalsIgnoreCase("GreenPlatypus8")) {
+                System.out.println("Name: " + studentB.getName());
+                System.out.println("Grades: " + studentB.getGrades());
+                System.out.println("Current Average: " + studentB.getGradeAverage());
+                System.out.println("Would you like to see another student?");
+                String keepGoing = scanner.nextLine();
+                if (keepGoing.equals("yes")) {
+                    continue;
+                } else {
+                    System.out.println("Goodbye! Have a nice day!");
+                    break;
+                }
+            } else if (teacherSelection.equalsIgnoreCase("VeggieTales9")) {
+                System.out.println("Name: " + studentC.getName());
+                System.out.println("Grades: " + studentC.getGrades());
+                System.out.println("Current Average: " + studentC.getGradeAverage());
+                System.out.println("Would you like to see another student?");
+                String keepGoing = scanner.nextLine();
+                if (keepGoing.equals("yes")) {
+                    continue;
+                } else {
+                    System.out.println("Goodbye! Have a nice day!");
+                    break;
+                }
+            } else if (teacherSelection.equalsIgnoreCase("NeonStars1")) {
+                System.out.println("Name: " + studentD.getName());
+                System.out.println("Grades: " + studentD.getGrades());
+                System.out.println("Current Average: " + studentD.getGradeAverage());
+                System.out.println("Would you like to see another student?");
+                String keepGoing = scanner.nextLine();
+                if (keepGoing.equals("yes")) {
+                    continue;
+                } else {
+                    System.out.println("Goodbye! Have a nice day!");
+                    break;
+                }
+            } else {
+                System.out.println("Sorry, no student found with the Github username of " + teacherSelection);
+                continue;
             }
-                else if(teacherSelection.equals("GreenPlatypus8 "))
-                {
-                    System.out.println("Name: " + studentB.getName());
-                    System.out.println("Grades: " + studentB.getGrades());
-                    System.out.println("Grade-point Average: " + studentB.getGradeAverage());
-                    }
-                    else if(teacherSelection.equals("VeggieTales9"))
-                        {
-                            System.out.println("Name: " + studentC.getName());
-                            System.out.println("Grades: " + studentC.getGrades());
-                            System.out.println("Grade-point Average: " + studentC.getGradeAverage());
-
-                            }
+        }
     }
 }
+
