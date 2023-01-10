fe343a20575459f2db74d998beab27a64c217506
Brittney Brinson <brittneyc95@outlook.com>
2023-01-09 23:00:37 -0500

    Cleaned up some code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,37 +1,35 @@
 import java.util.Scanner;
 public class HelloWorld {
     public static void main (String[] args){
-//        System.out.print("Hello, World!");
-//        int myFavoriteNumber = 3;
-//        System.out.print(myFavoriteNumber);
+        System.out.print("Hello, World!");
+        int myFavoriteNumber = 3;
+        System.out.print(myFavoriteNumber);
 
-//        String myString = "Chainsaw man is a great anime";
-//        System.out.println(myString);
-//
-//        double myNumber = 3.14;        //floats need an f character with the value
-////        System.out.println(myNumber);
-//        int x = 5;
-//        System.out.println(x++);
-//        System.out.println(x);
-//        int x = 5;
-//        System.out.println(++x);
-////        System.out.println(x);  // the first code block with the post increment, adds one to the variable after it's called. the second block adds one as it's called.
-//        String theNumberThree = "three";
-//        Object o = theNumberThree;
-//        int three = (int) o;
-//        System.out.println(three);
+        String myString = "Chainsaw man is a great anime";
+        System.out.println(myString);
 
-//        int x = 4 + 5;
-//        int y = 3 * 4;
+        double myNumber = 3.14;        //floats need an f character with the value
+       System.out.println(myNumber);
+        int x = 5;
+        System.out.println(x++);
+        System.out.println(x);
+        System.out.println(++x);
+        System.out.println(x);  // the first code block with the post increment, adds one to the variable after it's called. the second block adds one as it's called.
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+        System.out.println(three);
 
-//        double pi = 3.14159;
-//        System.out.format("The value of pi is approximately %.2f", pi);
+//       int x = 4 + 5;
+//       int y = 3 * 4;
+
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f", pi);
 
         Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter a integer: ");
+        System.out.print("Enter a integer: ");
 //        int userInput = scanner.nextInt();
-//
-//
+
         System.out.println("Enter a sentence");
         String userInput = scanner.nextLine();
         System.out.print(userInput);
--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,22 +1,22 @@
 public class StringExercise {
     public static void main(String[] args){
-//        String thing = "We don't need ";
-//
-//            System.out.println(thing + "no education");
-//
-//            System.out.println(thing + "no thought control");
-//
-//        String check = "Check \"this\"";
-//
-//            System.out.println(check +  " out!," + "\"s inside of \"s!");
-//
-//        String drive = "In windows, the main drive is ";
-//
-//            System.out.println(drive + "usually C:\\ ");
-//
-//        String bigBack = "I can do backslashes ";
-//
-//            System.out.println(bigBack + "\\, double backslashes \\\\, and the amazing triple backslash \\\\\\ !" );
+        String thing = "We don't need ";
+
+            System.out.println(thing + "no education");
+
+            System.out.println(thing + "no thought control");
+
+        String check = "Check \"this\"";
+
+            System.out.println(check +  " out!," + "\"s inside of \"s!");
+
+        String drive = "In windows, the main drive is ";
+
+            System.out.println(drive + "usually C:\\ ");
+
+        String bigBack = "I can do backslashes ";
+
+            System.out.println(bigBack + "\\, double backslashes \\\\, and the amazing triple backslash \\\\\\ !" );
 //
 
     }
